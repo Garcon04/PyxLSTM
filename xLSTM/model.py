117f2757fda71f393c43c79c6f23558bb8ea23f3
Garcon04 <151649617+Garcon04@users.noreply.github.com>
2024-09-07 17:22:48 +0700

    Update model.py

--- xLSTM/model.py
+++ xLSTM/model.py
@@ -29,7 +29,8 @@ class xLSTM(nn.Module):
             xLSTMBlock(input_size, hidden_size, num_layers, dropout, lstm_type)
             for _ in range(num_blocks)
         ])
-        self.output_layer = nn.GELU(input_size, 1)
+        self.output_layer = nn.GELU()
+        self.output_layer = nn.Linear(input_size, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_seq, hidden_states=None):
@@ -97,7 +98,8 @@ class CNN_xLSTM(nn.Module):
             xLSTMBlock(num_channels, hidden_size, num_layers, dropout, lstm_type)
             for _ in range(num_blocks)
         ])
-        self.output_layer = nn.GELU(num_channels, 1)
+        self.output_layer = nn.GELU()
+        self.output_layer = nn.Linear(num_channels, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_seq, hidden_states=None):
